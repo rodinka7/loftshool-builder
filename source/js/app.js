cdd0fcc1f15d73376a639e21a291c23ebb50097a
rodinka7 <rodinka7@gmail.com>
2016-06-13 01:48:09 +0300

    second commit

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,25 @@
 (function() {
   'use strict';
 
-  setTimeout(function() {
+  //Preloader
+  $(window).on('load', function () {
+    var $preloader = $('#page-preloader');
+    
+    $preloader.delay(1000).fadeOut('slow');
+  });
+  //Preloader-end
+
+  $('.autorize-link').on('click', function(e) {
+  	e.preventDefault();
+
+  	var flipper = $(this).closest('.flipper'),
+  		wrapper = $(this).closest('.wrapper-for-animation.hover');
+	wrapper.css({'transform' : 'rotateY(180deg)'});  	
+  	flipper.css({'transform' : 'rotateY(180deg)'});
+})
+  
+}
+  /*setTimeout(function() {
     document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+  }, 1000);*/
+)();
