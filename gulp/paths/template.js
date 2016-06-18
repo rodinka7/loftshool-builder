ed906435218e9a56fa3d003e5cdee22250271b17
rodinka7 <rodinka7@gmail.com>
2016-06-19 00:44:44 +0300

    Before deadline

--- gulp/paths/template.js
+++ gulp/paths/template.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = [
-  './source/template/welcome.jade',
+  './source/template/index.jade',
   './source/template/works.jade',
   './source/template/blog.jade',
   './source/template/about.jade'
