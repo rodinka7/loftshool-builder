4cad06714178c79fe36ad498d37d5b2805e70c13
rodinka7 <rodinka7@gmail.com>
2016-07-05 15:41:35 +0300

    Верстка админки - обо мне

--- gulp/paths/template.js
+++ gulp/paths/template.js
@@ -4,5 +4,6 @@ module.exports = [
   './source/template/index.jade',
   './source/template/works.jade',
   './source/template/blog.jade',
-  './source/template/about.jade'
+  './source/template/about.jade',
+  './source/template/admin.jade'
 ];
